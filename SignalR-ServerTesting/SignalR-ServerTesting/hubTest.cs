08e58aa5801be2589ecdae10b3acbbb43a46c38d
Naushad Warsi <warsinak@gmail.com>
2019-06-20 11:01:01 +0530

    working perfactly with proper ApiTested

---
+++ SignalR-ServerTesting/SignalR-ServerTesting/hubTest.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.SignalR.Client;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SignalR_ServerTesting
+{
+    public class HubTest
+    {
+        HubConnection connection;
+        public HubTest()
+        {
+            connection = new HubConnectionBuilder()
+                .WithUrl("https://localhost:44329/ChatHub")
+                .Build();
+             connection.StartAsync();
+        }
+
+        public async void  SendMethod(string msg)
+        {
+            await connection.InvokeAsync("SendMessage",
+                "abc", msg);
+        }
+
+    }
+}
